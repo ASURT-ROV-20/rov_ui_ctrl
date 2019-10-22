e7d0db25c2e34457c53fb1cb5e591cc0b9550625
Abanoub Ghadban <abanoub.gdb@gmail.com>
2019-10-22 21:46:22 +0200

    Display main window on start up

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <QApplication>
+#include "include/mainwindow.h"
 
 int main(int argc, char** argv) {
     QApplication a(argc, argv);
-    return 0;
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
