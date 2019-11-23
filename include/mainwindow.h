d5e2c8fcc756ae6e826220937e6ceddb6067100e
Abanoub Ghadban <abanoub.gdb@gmail.com>
2019-11-23 16:23:03 +0200

    added gstreamer

--- include/mainwindow.h
+++ include/mainwindow.h
@@ -7,6 +7,10 @@
 #include "JoyStick/joystick.h"
 #include "JoyStick/joystickpublisher.h"
 
+#include <glib-object.h>
+#include <gst/gst.h>
+#include <gst/video/video.h>
+
 namespace Ui {
 class MainWindow;
 }
@@ -21,15 +25,23 @@ public:
 
 private:
     Q_SLOT void handleTimer();
-    Q_SLOT void onAxisChanged(float x, float y, float z);
+    Q_SLOT void onAxisChanged(float x, float y, float z, float r);
     Q_SLOT void onJoystickConnected();
     Q_SLOT void onJoystickDisconnected();
     Q_SLOT void onButtonPressed(int btnNo);
     Q_SLOT void onButtonUp(int btnNo);
 
+    void initGstreamer();
+    void closeGstreamer();
+
     Ui::MainWindow *ui;
     unsigned int m_time;
     QTimer *m_timer;
+
+    GstElement* gst_pipline1;
+    GstElement* gst_sink1;
+    GstElement* gst_pipline2;
+    GstElement* gst_sink2;
 
     Joystick* m_joystick;
     JoystickPublisher* m_joystickPublisher;
--- source/mainwindow.cpp
+++ source/mainwindow.cpp
@@ -18,6 +18,8 @@ MainWindow::MainWindow(QWidget *parent) :
     connect(m_joystick, &Joystick::connected, this, &MainWindow::onJoystickConnected);
     connect(m_joystick, &Joystick::disconnected, this, &MainWindow::onJoystickDisconnected);
     m_joystickPublisher = new JoystickPublisher(m_joystick, this);
+
+    initGstreamer();
 }
 
 void MainWindow::handleTimer() {
@@ -27,8 +29,8 @@ void MainWindow::handleTimer() {
     ui->lblTimer->setText(minutes + ":" + seconds);
 }
 
-void MainWindow::onAxisChanged(float x, float y, float z) {
-    qDebug() << "Axis Changed: " << x << "," << y << "," << z;
+void MainWindow::onAxisChanged(float x, float y, float z, float r) {
+    qDebug() << "Axis Changed: " << x << "," << y << "," << z << ", " << r;
 }
 
 void MainWindow::onJoystickConnected() {
@@ -47,8 +49,31 @@ void MainWindow::onButtonPressed(int btnNo) {
     qDebug() << "Button Pressed: " << btnNo;
 }
 
+void MainWindow::initGstreamer() {
+    gst_init(nullptr, nullptr);
+    gst_pipline1 = gst_parse_launch("udpsrc port=5000 ! application/x-rtp,encoding-name=JPEG,payload=26 ! rtpjpegdepay ! jpegparse ! jpegdec ! videoconvert ! videoscale ! ximagesink  name=mySink", nullptr);
+    gst_sink1 = gst_bin_get_by_name((GstBin*)gst_pipline1,"mySink");
+
+    WId xwinid = ui->camera1Wdgt->winId();
+    gst_video_overlay_set_window_handle (GST_VIDEO_OVERLAY (gst_sink1), (guintptr)xwinid);
+    gst_element_set_state (gst_pipline1, GST_STATE_PLAYING);
+
+    gst_pipline2 = gst_parse_launch("udpsrc port=5001 ! application/x-rtp,encoding-name=JPEG,payload=26 ! rtpjpegdepay ! jpegparse ! jpegdec ! videoconvert ! videoscale ! ximagesink  name=mySink2", nullptr);
+    gst_sink2 = gst_bin_get_by_name((GstBin*)gst_pipline1,"mySink2");
+
+    WId xwinid2 = ui->camera2Wdgt->winId();
+    gst_video_overlay_set_window_handle (GST_VIDEO_OVERLAY (gst_sink2), (guintptr)xwinid2);
+    gst_element_set_state (gst_pipline2, GST_STATE_PLAYING);
+}
+
+void MainWindow::closeGstreamer() {
+    gst_object_unref(gst_pipline1);
+    gst_object_unref(gst_pipline2);
+}
+
 MainWindow::~MainWindow()
 {
+    closeGstreamer();
     if (m_joystick != nullptr) {
         m_joystick->close();
         delete m_joystick;
